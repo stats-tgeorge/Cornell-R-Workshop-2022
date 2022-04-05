728131d7498dac9e0d27b1ca5619e9e057d2ebfe
Tyler George <82473808+stats-tgeorge@users.noreply.github.com>
2022-04-05 14:09:54 -0500

    finish up workshop file

--- workshop_files/scripts/03-scrape-page-many.R
+++ workshop_files/scripts/03-scrape-page-many.R
@@ -7,12 +7,25 @@ library(glue)
 # list of urls to be scraped ---------------------------------------------------
 
 root <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset="
-numbers <- seq(from = 0, to = 2970, by = 10)
+# For time we are onyl going up to the first 70
+numbers <- seq(from = 0, to = 70, by = 10)
 urls <- glue("{root}{numbers}")
 
 # map over all urls and output a data frame ------------------------------------
 
 uoe_art <- map_dfr(urls, scrape_page)
+
+
+
+add_description <- function(indv_page_url){
+  read_html(indv_page_url) %>%
+    html_node("tr:nth-child(5) span") %>%
+    html_text()%>%
+    str_squish()
+}
+
+uoe_art <- uoe_art %>% mutate(description = map_chr(link,add_description))
+
 
 # write out data frame ---------------------------------------------------------
 
